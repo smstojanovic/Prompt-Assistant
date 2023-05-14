ee37fbb3fe4a55754886edc26adf9bd22f2805b9
smstojanovic <stojanovich.stevan@live.com>
2023-05-14 12:44:08 +1200

    text synthesizer working and providing sample embedding in json file. (#7)

--- src/python/assistant/apps/torchserve/build_script.py
+++ src/python/assistant/apps/torchserve/build_script.py
@@ -1,4 +1,9 @@
 import whisper
+from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
 
 whisper.load_model('tiny.en')
 whisper.load_model('base.en')
+
+processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
+model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
+vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
