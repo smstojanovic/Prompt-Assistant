24cc57e19ff91138a449f116fa3e339e0b5c2d28
smstojanovic <stojanovich.stevan@live.com>
2023-05-25 08:12:50 +0000

    adding in vad to build script.

--- src/python/assistant/apps/torchserve/build_script.py
+++ src/python/assistant/apps/torchserve/build_script.py
@@ -1,5 +1,6 @@
 import whisper
 from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
+from speechbrain.pretrained import VAD
 
 whisper.load_model('tiny.en')
 whisper.load_model('base.en')
@@ -7,3 +8,5 @@ whisper.load_model('base.en')
 processor = SpeechT5Processor.from_pretrained("microsoft/speecht5_tts")
 model = SpeechT5ForTextToSpeech.from_pretrained("microsoft/speecht5_tts")
 vocoder = SpeechT5HifiGan.from_pretrained("microsoft/speecht5_hifigan")
+
+vad = VAD.from_hparams(source="speechbrain/vad-crdnn-libriparty")
