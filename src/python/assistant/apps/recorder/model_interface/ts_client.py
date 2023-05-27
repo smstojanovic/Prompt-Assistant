04a5682da36247c871e1597625ae2a656a7a9aba
smstojanovic <stojanovich.stevan@live.com>
2023-05-27 12:27:27 +1200

    adding perceive client, with fixes to push data types depending on the model

--- src/python/assistant/apps/recorder/model_interface/ts_client.py
+++ src/python/assistant/apps/recorder/model_interface/ts_client.py
@@ -26,6 +26,10 @@ class TorchserveClient(ABC):
 
 class JenkinsPromptClient(TorchserveClient):
     MODEL_NAME = 'jenkins_prompt'
+    DTYPE = 'float32'
+
+    def process_result(self, result):
+        return result['transcribed_text']
 
     def do_inference(self, data, compressed):
         if type(data) != list:
@@ -36,18 +40,26 @@ class JenkinsPromptClient(TorchserveClient):
 
         json_data = {
             'raw_data' : data,
-            'dtype' : 'float32',
+            'dtype' : self.DTYPE,
             'compression' : compressed
         }
 
         result = self.send_request(json_data)
         if result is None:
             return None
-        return result['transcribed_text']
+        return self.process_result(result)
     
 class JenkinsListenClient(JenkinsPromptClient):
     #MODEL_NAME = 'jenkins_listen'
     MODEL_NAME = 'jenkins_prompt'
+
+class JenkinsPerceiveClient(JenkinsPromptClient):
+    #MODEL_NAME = 'jenkins_listen'
+    MODEL_NAME = 'jenkins_perceive'
+    DTYPE = 'int16'
+
+    def process_result(self, result):
+        return result['speech_segments']
 
 class JenkinsSpeechClient(TorchserveClient):
     MODEL_NAME = 'jenkins_speak'
