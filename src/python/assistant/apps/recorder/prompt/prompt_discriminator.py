405070e3433d2cd092002444b2c8551857b079da
smstojanovic <stojanovich.stevan@live.com>
2023-05-30 20:07:44 +1200

    changing assistant name

--- src/python/assistant/apps/recorder/prompt/prompt_discriminator.py
+++ src/python/assistant/apps/recorder/prompt/prompt_discriminator.py
@@ -1,19 +1,31 @@
+import itertools
+
+CALL_NAMES= ['Gaia','Guy Uh']
+
 INDICATORS = [
-    'OK Jenkins', 
-    'Hey Jenkins', 
-    'Hi Jenkins',
-    'A Jenkins',
-    'Eh Jenkins',
-    'Hello Jenkins',
+    'OK', 
+    'Hey', 
+    'Hi',
+    'A',
+    'Eh',
+    'Hello',
+    'Okay'
 ]
 
+import string
+
+def remove_punctuation(text):
+    translator = str.maketrans('', '', string.punctuation)
+    return text.translate(translator)
+
 class PromptDiscriminator:
-    def __init__(self, indicators=INDICATORS):
+    def __init__(self, indicators=INDICATORS, call_names=CALL_NAMES):
         self.indicators = [indicator.lower() for indicator in indicators]
+        self.call_names = [call_name.lower() for call_name in call_names]
     
     def check_prompt(self, text):
-        text_lower = text.lower()
-        for indicator in self.indicators:
-            if indicator in text_lower:
+        text_lower = remove_punctuation(text.lower())
+        for indicator, call_name in itertools.product(self.indicators,self.call_names):
+            if f'{indicator} {call_name}' in text_lower:
                 return True
         return False
